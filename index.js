178c8a7a8895e575a85872c0422c92c8ffd2f3ab
chavalk <chavalk@hotmail.com>
2020-12-17 01:36:03 -0600

    Added skeleton functions provided in assignment.

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 // Bringing in inquirer
 const inquirer = require('inquirer');
 
+// array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
