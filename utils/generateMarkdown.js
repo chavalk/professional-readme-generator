4167f73afce0898c679d23f0c49a48a93d9d98a6
chavalk <chavalk@hotmail.com>
2020-12-17 03:06:40 -0600

    Added sections to generateMarkdown function in generateMarkdown.js.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,28 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
     return `# ${data.title}
-  
+            ## Description
+            ${data.description}
+            ## Table of Contents
+            * [Installation](#installation)
+            * [Usage](#usage)
+            * [License](#license)
+            * [Contributing](#contributing)
+            * [Tests](#tests)
+            * [Questions](#questions)
+            ## Installation
+            ${data.installation}
+            ## Usage
+            ${data.usage}
+            ## License
+            ${data.license}
+            ## Contributing
+            ${data.contribution}
+            ## Tests
+            ${data.testing}
+            ## Questions
+            ${data.github}
+            ${data.email}
   `;
   }
   
