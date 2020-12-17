9143d379bfcbb342d7d537016c167e4d86ce8451
chavalk <chavalk@hotmail.com>
2020-12-17 02:03:24 -0600

    Added questions to questions array in index.js.

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const inquirer = require('inquirer');
 
 // array of questions for user
 const questions = [
-
+    "What is the title of your application?",
+    "What is the description of your application?",
+    "What are the installation instructions for your application?",
+    "What is the usage information for your application?",
+    "What are the contribution guidelines for your application?",
+    "What are the testing instructions for your application?",
+    "What is you GitHub username?",
+    "What is you email address?"
 ];
 
 // function to write README file
